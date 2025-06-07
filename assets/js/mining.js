193e3c8710320e660c6550659f7a67b0176e26ec
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-06-07 08:13:22 +0100

    Create mining.js

---
+++ assets/js/mining.js
@@ -0,0 +1,27 @@
+// mining.js
+class MiningSystem {
+    constructor() {
+        this.baseRate = 0.12 / (24 * 60 * 60 * 1000); // MZLx/ms
+        this.multipliers = {
+            free: 1.0,
+            bronze: 1.2,
+            silver: 1.5,
+            gold: 2.0
+        };
+        this.init();
+    }
+
+    init() {
+        document.getElementById('mining-toggle').addEventListener('click', () => {
+            this.toggleMining();
+        });
+    }
+    
+    toggleMining() {
+        if (this.isActive) {
+            this.stop();
+        } else {
+            this.start();
+        }
+    }
+}
