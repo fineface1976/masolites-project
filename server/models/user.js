08bb598be3c25bd1f60a4b31ec90ed25774bada7
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-06-10 16:16:38 +0100

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -35,3 +35,41 @@ userSchema.pre('save', function(next) {
 });
 
 module.exports = mongoose.model('User', userSchema)
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  memberId: { type: String, unique: true },
+  membership: {
+    type: String,
+    enum: ['FREE', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM'],
+    default: 'FREE'
+  },
+  purchasedTokens: { type: Number, default: 0 },
+  fiatWallet: { type: Number, default: 0 },
+  tokenWallet: { type: Number, default: 0 },
+  miningMultiplier: { type: Number, default: 1.0 },
+  upline: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  registrationDate: { type: Date, default: Date.now }
+});
+
+// Pre-save hook for member ID
+userSchema.pre('save', function(next) {
+  if (!this.memberId) {
+    const prefixMap = {
+      'FREE': 'MSL-F',
+      'BRONZE': 'MSL-B',
+      'SILVER': 'MSL-S',
+      'GOLD': 'MSL-G',
+      'PLATINUM': 'MSL-P'
+    };
+    const prefix = prefixMap[this.membership] || 'MSL-F';
+    const randomNum = Math.floor(10000000 + Math.random() * 90000000);
+    this.memberId = `${prefix}-${randomNum}`;
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
